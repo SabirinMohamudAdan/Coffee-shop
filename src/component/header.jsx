be8de6dbe15269fa12fcd11f548b89b3705303ab
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-01-28 09:26:06 -0800

    new update

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,6 +1,8 @@
 import { NavLink } from "react-router-dom"
 
 const Header=()=>{
+
+  
 return <>
 <div className="bg-[#59B8C3] w-[100%] h-[100px] pt-10  inset-0 fixed    sm:ml-0 ml-0  ">
     <ul className=" flex  gap-14 justify-center   text-white text-xl ">
